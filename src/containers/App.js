7c67a47234855eb31000e19fd11b22ae6519755b
Akinkuowo <akinseun1990@gmail.com>
2019-05-18 18:17:04 +0100

    updating app

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,42 +4,36 @@ import SearchBox from '../components/SearchBox';
 import CardList from '../components/CardList'
 import Scroll from  '../components/Scroll';
 import './app.css';
-import { setSearchField } from '../action';
+import { setSearchField, requestRobots } from '../action';
 
 const mapStateToProps = state => {
 	return{
-		searchField: state.searchField
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		pending: state.requestRobots.pending,
+		error: state.requestRobots.error
 	}
 }
 
 const mapDispatchToProps = (dispatch) => {
 	return{
-		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots())
 	}
 }
 
 class App extends React.Component{
- 	constructor(){
- 		super()
- 		this.state = {
- 		   robots: []
- 		}
- 		
- 	}
 
  	componentDidMount(){
-	 	fetch('http://jsonplaceholder.typicode.com/users')
-	 	.then(response => response.json())
-	 	.then(users => this.setState({ robots: users }));
+	 	this.props.onRequestRobots();
  	}
 
 	render() {
-		const {robots} = this.state;
-		const {searchField, onSearchChange} = this.props;
+		const {searchField, onSearchChange, robots, pending} = this.props;
 		const robotsFiltter = robots.filter(robots => {
  			return robots.name.toLowerCase().includes(searchField.toLowerCase());
  		})
- 		if(!robots.length){
+ 		if(pending){
  			return <h1 className="tc">Loading</h1>
  		}else{
  			return (
--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,34 @@
-import { CHANGE_SEARCH_FIELD } from './constants';
+import { CHANGE_SEARCH_FIELD, REQUEST_ROBOTS_PENDING, REQUEST_ROBOTS_SUCCESS, REQUEST_ROBOTS_FAILED } from './constants' ;
 
-const initialState = {
+const initialStateSearch = {
 	searchField: ''
 }
 
-export const searchRobots = (state=initialState, action={}) =>{
+
+export const searchRobots = (state=initialStateSearch, action={}) =>{
 	switch(action.type){
 		case CHANGE_SEARCH_FIELD:
 			return Object.assign({}, state, {searchField: action.payload});
+		default:
+			return state;
+	}
+}
+
+
+const initialStateRobots ={
+	pending: false,
+	robots: [],
+	failed: ''
+}
+
+export const requestRobots = (state=initialStateRobots, action={}) =>{
+	switch(action.type){
+		case REQUEST_ROBOTS_PENDING:
+			return Object.assign({}, state, {pending: true})
+		case REQUEST_ROBOTS_SUCCESS:
+			return Object.assign({}, state, {robots: action.payload, pending: false})
+		case REQUEST_ROBOTS_FAILED:
+			return Object.assign({}, state, {failed: action.payload, pending: false})
 		default:
 			return state;
 	}
