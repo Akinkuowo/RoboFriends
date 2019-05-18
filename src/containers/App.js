d99c170701da85a668d703ed8b5f33aa9da4510b
Akinkuowo <akinseun1990@gmail.com>
2019-05-18 16:01:19 +0100

    updating project

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,35 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import SearchBox from '../components/SearchBox';
 import CardList from '../components/CardList'
 import Scroll from  '../components/Scroll';
 import './app.css';
+import { setSearchField } from '../action';
 
- class App extends React.Component{
+const mapStateToProps = state => {
+	return{
+		searchField: state.searchField
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return{
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+	}
+}
+
+class App extends React.Component{
  	constructor(){
  		super()
  		this.state = {
- 		   robots: [],
- 		   searchfield: ''
+ 		   robots: []
  		}
  		
  	}
 
- componentDidMount(){
- 	fetch('http://jsonplaceholder.typicode.com/users')
- 	.then(response => response.json())
- 	.then(users => this.setState({ robots: users }));
- }
-
-
- 	onSearchChange = (event) => {
- 		this.setState({ searchfield: event.target.value })
- 		
+ 	componentDidMount(){
+	 	fetch('http://jsonplaceholder.typicode.com/users')
+	 	.then(response => response.json())
+	 	.then(users => this.setState({ robots: users }));
  	}
 
 	render() {
-		const {robots, searchfield} = this.state;
+		const {robots} = this.state;
+		const {searchField, onSearchChange} = this.props;
 		const robotsFiltter = robots.filter(robots => {
- 			return robots.name.toLowerCase().includes(searchfield.toLowerCase());
+ 			return robots.name.toLowerCase().includes(searchField.toLowerCase());
  		})
  		if(!robots.length){
  			return <h1 className="tc">Loading</h1>
@@ -37,7 +45,7 @@ import './app.css';
  			return (
 				<div className='tc'> 
 					<h1 className="tc f1">Robo Freinds</h1>
-					<SearchBox searchChange={this.onSearchChange} />
+					<SearchBox searchChange={onSearchChange} />
 					<Scroll>
 						<CardList robots={robotsFiltter} />
 					</Scroll>
@@ -48,4 +56,5 @@ import './app.css';
 	}	
 }
 
-export default App;
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
